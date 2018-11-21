3714ca52958af13927d89bad9ef406454904600e
songmenglong <songmenglong@gemvary.com>
2018-11-21 19:50:01 +0800

    blogs changes

---
+++ MyInsight_Blog/WelcomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  WelcomeVC.swift
+//  MyInsight_Blog
+//
+//  Created by gemvary_mini_2 on 2018/11/21.
+//  Copyright © 2018 SongMengLong. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
